5eb47ad46942afb61c70c40a25d3e8b4e126e809
lopress <90834282+lopress@users.noreply.github.com>
2023-06-01 13:21:14 +0200

    Fortschritt Schule 01.06.2023

--- Assets/MyGame/Scripts/InformationenAuftauchen.cs
+++ Assets/MyGame/Scripts/InformationenAuftauchen.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class InformationenAuftauchen : MonoBehaviour
 {
-    public GameObject erklaerText; 
+    public GameObject erklaerText;
+    public AudioClip soundClip;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = soundClip;
+        audioSource.playOnAwake = false;
+
+        erklaerText.SetActive(false);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) 
         {
-            erklaerText.SetActive(true); 
+            erklaerText.SetActive(true);
+            audioSource.Play();
         }
-        else
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
         {
+            audioSource.Stop();
             erklaerText.SetActive(false);
         }
     }
-}
 
+}
---
+++ Assets/MyGame/Scripts/SceneManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    void Start()
+    {
+        SceneSwitch();
+    }
+
+
+    private void SceneSwitch()
+    {
+        
+        //SceneManager.LoadScene("TestScene");
+        Debug.Log("Scene switcht");
+    }
+}
